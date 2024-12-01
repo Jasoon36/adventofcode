c9755e9d9556a74edc5e042ea6071569cfb5ab1c
Jason He <jason.he@bath.edu>
2024-12-01 17:40:06 +0000

    better solution to part 2

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -66,29 +66,12 @@ class Solution:
 
         left_list, right_list = list(map(list, zip(*input)))
 
-
-        left_count = {
-            left_num : left_list.count(left_num)
-                for left_num
-                in set(left_list)
-        }
-
-        right_count = {
-            right_num : right_list.count(right_num)
-                for right_num
-                in set(right_list)
-        }
-
-
         similarity_scores = [
-
-            int(left_key) * left_count * right_count[left_key]
-
-                for left_key, left_count
-                in left_count.items()
-                if left_key in right_count.keys()
-
+            int(left_key) * left_list.count(left_key) * right_list.count(left_key)
+                for left_key
+                in set(left_list)
         ]
+
 
         attempt = sum(similarity_scores)
 
