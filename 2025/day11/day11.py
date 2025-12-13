c61d6573bd08523f0a9f089201be873d8765faec
Jason He <jason.he@bath.edu>
2025-12-13 17:54:36 +0000

    2025: day 11 part 1, did i learn dfs and recurision?

---
+++ 2025/day11/day11.py
@@ -0,0 +1,110 @@
+class Solution:
+    '''
+        Attempt at Day 11
+    '''
+    def __init__(self):
+        self.year           = '2025'
+        self.day            = '11'
+        self.prod           = self.read('input.txt')
+        self.test           = self.read('input_test.txt')
+        # self.test1Ans       = []
+        # self.test2Ans       = []
+        self.part1TestAns   = 5
+        self.part2TestAns   = 0
+
+
+    def read(self, filename: str) -> list:
+        with open(f'./{self.year}/day{self.day}/{filename}') as f:
+            input = [line.strip('\n') for line in f]
+
+        return input
+    
+    
+    def part1(self, realAttempt = False) -> int:
+
+        if realAttempt:
+            input = self.prod
+        else:
+            input = self.test
+
+        rack = {
+            (key_value := line.split(': '))[0] : key_value[1].split(' ')
+                for line
+                in input
+        }
+
+        rack_paths = {}
+
+        def getPaths(device: str) -> int:
+            
+            paths = sum(
+                1 if next_device == 'out' else rack_paths.setdefault(next_device, getPaths(next_device))
+                    for next_device
+                    in rack[device]
+            )
+
+            return paths
+
+        return getPaths('you')
+        
+    def runPart1(self):
+
+        try:
+            part1TestAttempt = self.part1()
+            assert part1TestAttempt == self.part1TestAns
+        except AssertionError as e:
+            e.add_note(f'part 1 test ans {part1TestAttempt} is not {self.part1TestAns}')
+            raise e
+        
+        realAttempt = True
+        print(self.part1(realAttempt))
+
+
+
+    def solve2(self, line: str) -> int:
+
+        lineAns = 0
+
+        return lineAns
+
+    def testSolution2(self) -> bool:
+
+        for line, ans in zip(self.test, self.test2Ans):
+            try:
+                attempt = self.solve2(line)
+                assert attempt == ans
+            except AssertionError as e:
+                e.add_note(f'{attempt} is not {ans} for input\n{line}')
+                raise e
+        
+        print('keep going')
+        return True
+    
+    def part2(self, realAttempt = False) -> int:
+
+        if realAttempt:
+            input = self.prod
+        else:
+            input = self.test
+
+        attempt = sum([1 for line in input])
+
+        return attempt
+        
+    def runPart2(self):
+
+        try:
+            part2TestAttempt = self.part2()
+            assert part2TestAttempt == self.part2TestAns
+            print('yay')
+        except AssertionError as e:
+            e.add_note(f'part 2 test ans {part2TestAttempt} is not {self.part2TestAns}')
+            raise e
+        
+        realAttempt = True
+        print(self.part2(realAttempt))
+
+
+a = Solution()
+a.runPart1()
+a.runPart2()
